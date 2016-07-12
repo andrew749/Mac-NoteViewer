78952c470e8140d12c88d9d360efaed523747a45
Andrew Codispoti <andrewcod749@gmail.com>
2016-07-12 00:58:20 -0400

    Initial Commit

---
+++ NoteViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NoteViewer
+//
+//  Created by Andrew Codispoti on 2016-07-12.
+//  Copyright © 2016 andrewcodispoti. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NoteViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NoteViewer
+//
+//  Created by Andrew Codispoti on 2016-07-12.
+//  Copyright © 2016 andrewcodispoti. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
